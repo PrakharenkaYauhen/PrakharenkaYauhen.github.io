f48f9496329b770c572346d2e83e6d8ab1276da0
PrakharenkaYauhen <31861263+PrakharenkaYauhen@users.noreply.github.com>
2018-10-02 19:43:56 +0300

    Library for the Task N 37

---
+++ countries.js
@@ -0,0 +1,34 @@
+//Ничего не изменять!!!
+
+var countries={}; // пустой объект
+
+// добавление информации о стране
+function addCountry(countryName, capitalName) {
+	countries[countryName]=capitalName;
+}
+
+// удаление страны по ее названию
+function deleteCountry(countryName) {
+	delete countries[countryName];
+}
+
+// получение информации о стране и возвращение строки с этой информацией или ошибкой
+function getCountryInfo(сountryName) {
+	if ( сountryName in сountries ) {
+		return ('страна: ' + сountryName + ' столица: ' + сountries[сountryName]);
+	}
+	else {
+		return ('нет информации о стране ' + сountryName + '!');
+	}
+}
+
+// вывод объекта в строку, с переносами
+function listCountries() {
+	var result="";
+
+	for ( var key in countries ) {
+		result+='\n'+getCountryInfo(key);
+	}
+
+	return result;
+}
